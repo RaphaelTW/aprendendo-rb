9c96d00426254471f3928176504cb21d1dfb00a1
RaphaelTW <raphael.tw22@gmail.com>
2025-02-17 22:33:11 -0300

    boleanos

---
+++ 03_condicionais/01_boleanos.rb
@@ -0,0 +1,23 @@
+'''
+AND
+Verdade e Verdade = verdade
+Verdade e Falso = falso
+Falso e Falso = falso
+Falso e Verdade = falso
+
+OR
+Verdade ou Verdade = verdade
+Verdade ou Falso = verdade
+Falso ou Falso = falso
+Falso ou Verdade = verdade
+
+NOT
+Verdade não = falso
+Falso não = verdade
+'''
+
+puts false && true
+puts false || true
+puts !false && true
+puts !false
+puts !true
