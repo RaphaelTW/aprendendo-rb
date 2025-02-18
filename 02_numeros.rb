b40d0a06bd9a82870b25ada89a812fcf0c09f8e4
RaphaelTW <raphael.tw22@gmail.com>
2025-02-17 22:04:42 -0300

    números e somas

---
+++ 02_numeros.rb
@@ -0,0 +1,17 @@
+puts "Digite um números:"
+x = gets.chomp.to_i
+puts "Digite outro número:"
+y = gets.chomp.to_i
+
+# soma = x + y
+# puts soma
+
+soma = x + y
+divisao = x / y
+multiplicacao = x * y
+subtracao = x - y
+
+puts "Soma: #{soma}"
+puts "Divisão: #{divisao}"
+puts "Multiplicação: #{multiplicacao}"
+puts "Subtração: #{subtracao}"
