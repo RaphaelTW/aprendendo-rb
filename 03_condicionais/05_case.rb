93a93bf698157dbbb61bede9ceced785bfa4986b
RaphaelTW <raphael.tw22@gmail.com>
2025-02-17 22:52:30 -0300

    condicionais cases

---
+++ 03_condicionais/05_case.rb
@@ -0,0 +1,11 @@
+mes = "Maio"
+case mes
+  when "Maio"
+    puts "tem 31 dias"
+  when "Fevereiro"
+    puts "tem 28 dias"
+  when "Março"
+    puts "tem 30 dias"
+  else
+    puts "Não sei quantos dias tem esse mês"
+end
